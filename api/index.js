a0b4e03f841ec4820a5920034cb7d9ab7ea2cb4a
Atharv Kulkarni <atharvkulkarni429@gmail.com>
2024-05-26 09:44:43 +0530

    Update for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -210,7 +210,9 @@ app.get('/bookings', async (req, res) => {
     res.json(await Booking.find({user:userData.id}).populate('place'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
